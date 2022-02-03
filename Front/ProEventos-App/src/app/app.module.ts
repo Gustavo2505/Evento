4277d618b620f11949101d8be9ac31a465176fd9
Gustavo Henrique <37144808+Gustavo2505@users.noreply.github.com>
2022-02-03 20:55:29 -0300

    Add Front project

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventosComponent } from './Eventos/Eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EventosComponent } from './eventos/eventos.component';
+
+@NgModule({
+  declarations: [				
+    AppComponent,
+      EventosComponent,
+      PalestrantesComponent,
+      EventosComponent,
+      EventosComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
